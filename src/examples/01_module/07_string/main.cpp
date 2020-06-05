361babf9413266aec05563e8f0c3cf12515f3060
Mr-Bari-99 <brandon.hubacher@gmail.com>
2020-06-04 20:23:14 -0500

    Examples module 1

--- src/examples/01_module/07_string/main.cpp
+++ src/examples/01_module/07_string/main.cpp
@@ -1,8 +1,15 @@
-
+//string is part of standard library, so we have to #include
+#include<iostream>
+#include<string>
+using std::string;
 
 int main() 
 {
-	//Create string variable and display it		
+	//Create string variable and display it
+	std::string name = "John";
+	string last_name = "Doe";
+
+	std::cout<<name<<" "<<last_name;
 
 	return 0;
 }
