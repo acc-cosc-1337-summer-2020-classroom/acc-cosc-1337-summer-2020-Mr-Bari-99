c40b9ea9883c9f74e02704873382759133fb8669
Mr-Bari-99 <brandon.hubacher@gmail.com>
2020-06-04 21:15:58 -0500

    Assignment 1 try 2

--- test/classwork_test/01_assign_test/types_tests.cpp
+++ test/classwork_test/01_assign_test/types_tests.cpp
@@ -1,7 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "types.h"
-//hh
+//hi
+//hope this works
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
