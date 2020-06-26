7ff8db00db88581d8df957eaa72579d958c5107c
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-06-25 20:21:29 -0500

    errors for some reason?

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -3,10 +3,18 @@
 
 bool TicTacToe::game_over()
 {
-
+    return check_board_full();
 }
 
 bool TicTacToe::check_board_full()const
 {
+    for(auto peg: pegs)
+    {
+        if(peg == " ")
+        {
+            return false;
+        }
+    }
 
+    return true;
 }
